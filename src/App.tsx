320a56ce79b867fe694b08aaa534e81d2f078a71
Sheikh Bahauddin Arnab <arnab.sheikh007@gmail.com>
2023-12-01 14:52:37 +0600

    Created NavBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,33 @@
 import { useState } from "react";
 
-function App() {
-  const [darkMode, setDarkMode] = useState(false);
+import Navbar from "./components/Navbar";
+const App: React.FC = () => {
+  const [darkMode, setDarkMode] = useState<boolean>(true);
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const toggleMenu = (): void => {
+    setIsOpen((prev) => !prev);
+  };
+
+  const toggleTheme = (): void => {
+    setDarkMode((prev) => !prev);
+  };
   return (
     <div
       className={`w-full h-full min-h-[100vh] bg-white ${darkMode && "dark"}`}
     >
       <div className="w-full h-full min-h-[100vh] bg-white ">
         <div className="dark:bg-gradient-to-tr from-[#0235a3] via-[#030a1c] to-[#05174e]">
-          Portfolio
+          <Navbar
+            darkMode={darkMode}
+            isOpen={isOpen}
+            toggleMenu={toggleMenu}
+            toggleTheme={toggleTheme}
+          />
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default App;
