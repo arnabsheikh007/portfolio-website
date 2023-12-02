166ba2d42089fa7a6bab01f58ecab6d7381d8549
Sheikh Bahauddin Arnab <arnab.sheikh007@gmail.com>
2023-12-02 23:10:58 +0600

    Added Projects

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Header from "./components/Header";
 import Works from "./components/Works";
 import About from "./components/About";
 import Skills from "./components/Skills";
+import Projects from "./components/Projects";
 const App: React.FC = () => {
   const [darkMode, setDarkMode] = useState<boolean>(true);
   const [isOpen, setIsOpen] = useState<boolean>(false);
@@ -46,6 +47,12 @@ const App: React.FC = () => {
           className="w-full px-0 lg:px-5 2xl:px-40 py-10 lg:py-0 dark:bg-[radial-gradient(ellipse_at_top_left,_var(--tw-gradient-stops))] from-[#05174e] to-[#030a1c]"
         >
           <Skills />
+        </section>
+        <section
+          id="projects"
+          className="w-full px-0 lg:px-5 2xl:px-40 py-10 lg:py-0 dark:bg-[radial-gradient(ellipse_at_top_left,_var(--tw-gradient-stops))] from-[#030a1c] to-[#05174e]"
+        >
+          <Projects />
         </section>
       </div>
     </div>
